cf958e0263e43fff983a579d7d24fdccef01af4d
Hailey Lee <hklee7856@gmail.com>
2025-10-09 23:16:22 -0700

    Add files via upload

---
+++ week 3/reading2.js
@@ -0,0 +1,14 @@
+var yashName = "Hailey";
+let location = " Los Angeles";
+const weather = 'sunny';
+let year = 2025;
+let yashHappy = true;
+let person = {hometown: "Seoul", hobbies: ['running, cooking, traveling']};
+
+person.name = yashName;
+person.happy = yashHappy;
+person.eyes = "Brown"
+person.homes = {location: "Seoul"}
+person.hobbies = {}
+
+console.log(person);
